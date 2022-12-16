c973bf89cecad228b3bde0da285b4a27d6f925cf
gyeongza <gyeongza@naver.com>
2022-12-16 19:30:31 +0900

    feat : 다리 길이를 입력 받는 기능(재구현)

--- src/retry/App.js
+++ src/retry/App.js
@@ -1,12 +1,19 @@
+const BridgeGame = require("./BridgeGame");
+const InputView = require("./InputView");
 const OutputView = require("./OutputView");
 
 class App {
   play() {
-    OutputView.printStart()
+    OutputView.printStart();
+    this.getBridgeSize();
+  }
+  getBridgeSize() {
+    const bridgeGame = new BridgeGame();
+    InputView.readBridgeSize(bridgeGame);
   }
 }
 
-const app = new App()
-app.play()
+const app = new App();
+app.play();
 
 module.exports = App;
